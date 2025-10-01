6e8b5db35750331a5ed6d5ac4b31f26817d0e3d2
90828 <908288050@qq.com>
2025-10-01 16:39:00 +0800

    去除多级loss

---
+++ work2_1/model/backbone_my2.py
@@ -0,0 +1,187 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from model import BasicConv
+from .layers import *
+
+
+class EBlock(nn.Module):
+    def __init__(self, channels):
+        super(EBlock, self).__init__()
+        layers = [ResBlock(channels, channels, "ITS") for _ in range(8 - 1)]
+        layers.append(ResBlock(channels, channels, "ITS", filter=True))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class DBlockPred(nn.Module):
+    def __init__(self, channels):
+        super(DBlockPred, self).__init__()
+        layers = [ResBlock(channels, channels, "ITS") for _ in range(8 - 1)]
+        layers.append(ResBlock(channels, channels, "ITS", filter=True))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+        # return x
+
+
+class BottleNeck(nn.Module):
+    def __init__(self, channels):
+        super(BottleNeck, self).__init__()
+        self.convMlp = nn.Sequential(
+            BasicConv(channels, channels // 2, kernel_size=1, stride=1, relu=True),
+            BasicConv(channels // 2, channels, kernel_size=1, stride=1, relu=False)
+        )
+
+    def forward(self, x):
+        return self.convMlp(x) + x
+
+class DBlockFlare(nn.Module):
+    def __init__(self, channels):
+        super(DBlockFlare, self).__init__()
+        layers = [ResBlock(channels, channels, "ITS") for _ in range(8 - 1)]
+        layers.append(ResBlock(channels, channels, "ITS", filter=True))
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class ConvS(nn.Module):
+    def __init__(self, out_channels):
+        super(ConvS, self).__init__()
+        self.main = nn.Sequential(
+            BasicConv(3, out_channels // 4, kernel_size=3, stride=1, relu=True),
+            BasicConv(out_channels // 4, out_channels // 2, kernel_size=1, stride=1, relu=True),
+            BasicConv(out_channels // 2, out_channels // 2, kernel_size=3, stride=1, relu=True),
+            BasicConv(out_channels // 2, out_channels, kernel_size=1, stride=1, relu=False),
+            nn.InstanceNorm2d(out_channels, affine=True)
+        )
+
+    def forward(self, x):
+        x = self.main(x)
+        return x
+
+
+# gaussian transform block
+def GTB(x, layer=4):
+    res_gaussian = []
+    res_laplacian = []
+    kernel = torch.tensor([[1, 4, 6, 4, 1],
+                           [4, 16, 24, 16, 4],
+                           [6, 24, 36, 24, 6],
+                           [4, 16, 24, 16, 4],
+                           [1, 4, 6, 4, 1]], dtype=torch.float32, device=x.device) / 256.0
+    b, c, h, w = x.shape
+    kernel = kernel.view(1, 1, 5, 5).repeat(c, 1, 1, 1)
+    current = x
+    res_gaussian.append(current)
+    for i in range(layer):
+        blurred = F.conv2d(current, kernel, padding=2, groups=c)
+        blurred = blurred[:, :, ::2, ::2]
+        res_gaussian.append(blurred)
+        upsampled = F.interpolate(blurred, size=current.shape[2:], mode='bilinear', align_corners=False)
+        laplacian = current - upsampled
+        res_laplacian.append(laplacian)
+        current = blurred
+    return res_gaussian, res_laplacian
+
+
+class DownSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DownSample, self).__init__()
+        self.down = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = self.down(x)
+        return self.conv(x)
+
+
+class UpSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UpSample, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = self.up(x)
+        return self.conv(x)
+
+
+class MyNet2(nn.Module):
+    def __init__(self, base_channels=16, num_block=3, num_bottleneck=2):
+        super(MyNet2, self).__init__()
+        self.num_block = num_block
+        self.num_bottleneck = num_bottleneck
+        self.proj_in = BasicConv(3, base_channels, kernel_size=3, padding=1)
+        self.in_reduce = nn.ModuleList()
+        self.out_reduce = nn.ModuleList()
+        for i in range(1,num_block):
+            self.in_reduce.append(BasicConv(base_channels * 2 ** (i+1), base_channels * 2 ** i, kernel_size=3, padding=1))
+            self.out_reduce.append(BasicConv(base_channels * 2 ** i, base_channels * 2 ** (i-1), kernel_size=3, padding=1))
+        self.out_reduce.append(BasicConv(base_channels * 2 ** num_block, (base_channels * 2 ** (num_block-1)), kernel_size=3, padding=1))
+        self.intro = nn.ModuleList()
+        for i in range(1, num_block):
+            self.intro.append(ConvS(base_channels * 2 ** i))
+        self.proj_laplacian = nn.ModuleList([BasicConv(3, base_channels * 2 ** (i), kernel_size=3, padding=1) for i in
+                               range(num_block)])
+        self.ebs = nn.ModuleList([EBlock(base_channels * 2 ** i) for i in range(num_block)])
+        self.bottleneck = nn.ModuleList([BottleNeck(base_channels * 2 ** (num_block)) for _ in range(num_bottleneck)])
+        self.dbs_pred = nn.ModuleList([DBlockPred(base_channels * 2 ** (i)) for i in range(num_block)])
+        self.dbs_flare = nn.ModuleList([DBlockFlare(base_channels * 2 ** (i)) for i in range(num_block)])
+
+        self.ups = nn.ModuleList([UpSample(base_channels * 2 ** (i+1), base_channels * 2 ** i) for i in range(num_block)])
+        self.downs = nn.ModuleList([DownSample(base_channels * 2 ** i, base_channels * 2 ** (i+1)) for i in range(num_block)])
+        self.projout = BasicConv(base_channels, 6, kernel_size=3, padding=1)
+    def scale(self,x, factor):
+        _, _, h, w = x.size()
+        new_h = int(h * factor)
+        new_w = int(w * factor)
+        if(new_h == h) and (new_w == w):
+            return x
+        return F.interpolate(x, size=(new_h, new_w), mode='bilinear', align_corners=False)
+
+    def forward(self, x):
+        skip = []
+        gauss, laplacian = GTB(x, layer=3)
+        res = self.proj_in(gauss[0])
+
+        for i in range(1, self.num_block):
+            res = self.ebs[i - 1](res)
+            skip.append(res)
+            res = self.downs[i - 1](res)
+            ain = self.intro[i - 1](gauss[i])
+            res = torch.cat((res, ain), dim=1)
+            res = self.in_reduce[i - 1](res)
+
+        res = self.ebs[-1](res)
+        skip.append(res)
+        res = self.downs[-1](res)
+        for i in range(self.num_bottleneck):
+            res = self.bottleneck[i](res)
+
+        for i in range(0, self.num_block):
+            res = self.ups[-1-i](res)
+            aout = self.proj_laplacian[-1 - i](laplacian[-1 - i])
+            res = self.dbs_flare[-1 - i](res + aout)
+            res = torch.cat((res, skip[-1 - i]), dim=1)
+            res = self.out_reduce[-1 - i](res)
+            res = self.dbs_pred[-1 - i](res)
+        res = self.projout(res)
+        pred,flare = torch.chunk(res,2,dim=1)
+        return pred+x,flare
+
+#
+if __name__ == '__main__':
+    model = MyNet2(base_channels=16)
+    x = torch.randn(2, 3, 384, 384)  # Batch size of 1, 3 channels, 512x512 image
+    pred, flare = model(x)
+    print(pred, flare)  # Should be (1, 3, 512, 512)
