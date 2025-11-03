f90d04408d5cd09fd84d1a81607d29398b5ccafc
90828 <908288050@qq.com>
2025-11-03 15:15:26 +0800

    不concat

--- work2_1/model/backbone_my2_3.py
+++ work2_1/model/backbone_my2_3.py
@@ -166,7 +166,7 @@ class MyNet2_3(nn.Module):
 
     def forward(self, x):
         skip = []
-        gauss, laplacian = GTB(x, layer=3)
+        gauss, laplacian = GTB(x, layer=self.num_block)
         res = self.proj_in(x)
 
         for i in range(0, self.num_block):
--- work2_1/temp.py
+++ work2_1/temp.py
@@ -1,14 +1,14 @@
 import torch
 
-from model import MyNet2_2, UBlock
+from model import MyNet2_3
 from torchsummary import summary
 
 from utils import MinIOHelper
 import os
 
 if __name__ == '__main__':
-    model = MyNet2_2(base_channels=4).cuda()
-    x = torch.randn(1,3,128,128).cuda()
+    model = MyNet2_3(base_channels=4).cuda()
+    x = torch.randn(1,3,256,256).cuda()
     y=model(x)
     print(model)
 
